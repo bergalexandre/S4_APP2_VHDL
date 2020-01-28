ea303dc55e33ad72bbe52a7d68cc19ae8a5f9a86
Mathieu Parent <mathieu.parent4@usherbrooke.ca>
2020-01-27 22:32:30 -0500

    yolo commande

--- pb_APP_log_seq.srcs/sources_1/new/module_commande.vhd
+++ pb_APP_log_seq.srcs/sources_1/new/module_commande.vhd
@@ -35,13 +35,16 @@ end component;
 
 
     type fsm_etat_aff IS (S1,S2,S3,S4);
-    SIGNAL fsm_aff, fsm_aff_suiv : fsm_etat_aff;
+    SIGNAL fsm_aff, fsm_aff_suiv : fsm_etat_aff := S1;
     signal d_strobe_btn :    std_logic_vector (nbtn-1 downto 0);
     signal d_btn_cd     :    std_logic_vector (nbtn-1 downto 0); 
     signal d_reset      :    std_logic;
+    signal o_btn0       :    std_logic_vector(1 downto 0);
+    signal btn0         :    std_logic;
+    signal clk_btn0     :    std_logic;
    
 BEGIN 
-
+    
                   
  inst_cond_btn:  conditionne_btn_v7
     generic map (nbtn => nbtn, mode_simul => mode_simulation)
@@ -52,8 +55,56 @@ BEGIN
         o_strobe_btn  => d_strobe_btn  
          );
  
+  MEF_btn0: process(clk_btn0)
+    begin
+        case fsm_aff is
+            when S1 =>
+                if d_btn_cd(0) = '1' then
+                        fsm_aff_suiv <= S2;
+                    else
+                        fsm_aff_suiv <= S1;
+                end if;
+            when S2 =>
+                if d_btn_cd(0) = '1' then
+                        fsm_aff_suiv <= S3;
+                    else
+                        fsm_aff_suiv <= S2;
+                end if;
+            when S3 =>
+                if d_btn_cd(0) = '1' then
+                        fsm_aff_suiv <= S4;
+                    else
+                        fsm_aff_suiv <= S3;
+                end if;
+            when S4 =>
+                if d_btn_cd(0) = '1' then
+                        fsm_aff_suiv <= S1;
+                    else
+                        fsm_aff_suiv <= S4;
+                end if;
+        end case;
+    end process;
+    
+    
+    MEF_output: process(clk_btn0)
+    begin
+        case fsm_aff is
+            when S1 =>
+                o_btn0 <= "00";
+            when S1 =>
+                o_btn0 <= "01";
+            when S1 =>
+                o_btn0 <= "10";
+            when S1 =>
+                o_btn0 <= "11";
+        end case;
+    end process;
+ 
+    clk_btn0 <= not clk_btn0 after 50ms;
+
+ 
    o_btn_cd        <= d_btn_cd;
-   o_selection_par <= i_sw(1 downto 0); -- mode de selection du parametre par sw
+   o_selection_par <= o_btn0; -- mode de selection du parametre par sw
    o_selection_fct <= i_sw(3 downto 2); -- mode de selection de la fonction par sw
    d_reset         <= i_btn(3);         -- pas de contionnement particulier sur reset
    o_reset         <= d_reset;          -- pas de contionnement particulier sur reset
